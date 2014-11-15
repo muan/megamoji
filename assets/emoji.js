810210508e75d2aeaaeac0dc887cdb5ca02d0b97
Mu-An Chiou <me@muanchiou.com>
2014-11-16 05:28:44 +0800

    trying to rewrite everything again

---
+++ assets/emoji.js
@@ -0,0 +1,64 @@
+$(document).on('ready', prepareEmoji)
+
+$(document).on('click', function(e) {
+  hideDropdownMaybe(e)
+})
+
+$(document).on('focus', 'input', function(e) {
+  hideDropdownMaybe(e)
+})
+
+// Load emoji through json file and create an invisible dropdown
+function prepareEmoji () {
+  $('body').append('<div class="emoji-container js-emoji-container">')
+
+  $.getJSON('emojis.json', function (emojis, s) {
+    $.each(emojis, function (name, keywords) {
+      var emoji = ':'+name+':'
+      $('.js-emoji-container').append("<div class='emoji js-emoji' data-keywords='" + emoji + ", " + keywords + "'><img alt='" + emoji + "' title='" + emoji + "' src='/emojis/" + name + ".png'>" + emoji + "</div>")
+    })
+  })
+}
+
+function hideDropdownMaybe (e) {
+  if(!$(e.target).hasClass('js-auto-emoji') && !$(e.target).hasClass('js-emoji')) {
+    $('.js-emoji-container').hide()
+  }
+}
+
+// Search
+$(document).on('input', '.js-auto-emoji', function() {
+  var target = $(this)
+  var term = target.val()
+  $('.js-emoji').hide()
+  $('.js-emoji').each(function () {
+    if($(this).attr('data-keywords').indexOf(term) >= 0) {
+      $(this).show()
+    }
+  })
+})
+
+$(document).on('blur', '.js-auto-emoji', function() {
+  if(this.value === '') this.value =  this.originalVal
+})
+
+// Show dropdown
+$(document).on('focus', '.js-auto-emoji', function() {
+  var target = $(this)
+  this.originalVal = target.val()
+  target.val('')
+  $(document).off('click', '.js-emoji')
+  $('.js-emoji').show()
+
+  var dropdown = $('.js-emoji-container')
+  dropdown.css('top', $(this).offset().top + $(this).outerHeight() + 'px')
+  dropdown.css('left', $(this).offset().left)
+
+  $(document).on('click', '.js-emoji', function () {
+    target.val($(this).find('img').attr('alt'))
+    target.trigger('selected')
+    dropdown.hide()
+  })
+
+  dropdown.show()
+})
---
+++ assets/func2.js
@@ -0,0 +1,82 @@
+$(document).on('ready', function() {
+  setGrid()
+  setEmojiPaint($('.js-paint').val())
+})
+
+// drag
+$(document).on('mousedown', '.cell', function (e) {
+  boolean = !$(e.target).hasClass('painted')
+  markSelected($(e.target), boolean)
+  $(document).on('mouseover', '.cell', function (e) {
+    markSelected($(e.target), boolean)
+  })
+})
+
+// drop
+$(document).on('mouseup', '.cell', function (e) {
+  $(document).off('mouseover', '.cell')
+})
+
+$(document).on('change', '.js-grid-rows, .js-grid-cols', function() {
+  setGrid()
+})
+
+// select an emoji as paint
+$(document).on('selected', '.js-paint', function () {
+  setEmojiPaint($(this).val())
+})
+
+$(document).on('selected', '.js-set-emoji-background', function() {
+  setEmojiBackground($('.grid'), $(this).val())
+})
+
+$(document).on('change', '.js-set-file-background', function () {
+  var reader = new FileReader()
+  reader.onload = function (e) {
+    $('.grid').css('background-image', 'url("' + e.target.result + '")')
+  }
+  reader.readAsDataURL(this.files[0])
+})
+
+function setEmojiPaint (emoji) {
+  var emoji = emoji.replace(/:/g, '')
+  $('.js-paint-preview').css('background-image', 'url("emojis/' + emoji + '.png")')
+}
+
+function setGrid () {
+  if($('.cell.painted').length > 0) {
+    if(!confirm('Resetting the grid will clear the canvas, are you sure?')) {
+      return
+    }
+  }
+
+  var rows = Number($('.js-grid-rows').val())
+  var cols = Number($('.js-grid-cols').val())
+  var grid = $('.js-grid')
+  var gridWidth = grid.width()
+  var cellSize = gridWidth/cols
+  grid.html('')
+
+  for(i=0; i < (cols*rows); i++) {
+    grid.append("<div class='cell' style='width: " + cellSize + "px; height: " + cellSize + "px;'>")
+  }
+  // resetAll()
+}
+
+function markSelected (ele, toggle) {
+  if(typeof toggle === 'undefined') { var toggle = true }
+
+  if(toggle) {
+    ele.addClass('painted')
+    setEmojiBackground(ele, $('.js-paint').val())
+  } else {
+    ele.removeClass('painted')
+    ele.css('background-image', 'none')
+  }
+}
+
+// helpers
+function setEmojiBackground(target, emoji) {
+  var emoji = emoji.replace(/:/g, '')
+  target.css('background-image', 'url("emojis/' + emoji + '.png")')
+}
